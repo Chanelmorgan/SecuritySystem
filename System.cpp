65cc33c7eb9962753e217ad3f143f579829351c1
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-26 12:32:38 +0000

    Creating the system class with the headers adn the variables

---
+++ System.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Chanel Morgan on 26/02/2024.
+//
+
+#include "System.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    // variables
+    int a, i=0;
+    string text, old, password1, password2, pass, name, password0, age, user, word, word1;
+    string creds[2], cp[2];
+
+
+}
+
+
---
+++ System.h
@@ -0,0 +1,14 @@
+//
+// Created by Chanel Morgan on 26/02/2024.
+//
+
+#ifndef SECURITYSYSTEM_SYSTEM_H
+#define SECURITYSYSTEM_SYSTEM_H
+
+
+class System {
+
+};
+
+
+#endif //SECURITYSYSTEM_SYSTEM_H
